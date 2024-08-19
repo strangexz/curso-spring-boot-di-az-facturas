d8ef8383631792a220f6d30cb858d3a4a51918c5
Strangexz <ja_figueroa@outlook.es>
2024-08-19 17:01:59 -0600

    refactor: tageando modelos como componentes

--- src/main/java/hn/com/jf/models/Client.java
+++ src/main/java/hn/com/jf/models/Client.java
@@ -1,7 +1,10 @@
 package hn.com.jf.models;
 
+import org.springframework.stereotype.Component;
+
 import lombok.Data;
 
+@Component
 @Data
 public class Client {
 	private String name;
--- src/main/java/hn/com/jf/models/Invoice.java
+++ src/main/java/hn/com/jf/models/Invoice.java
@@ -2,10 +2,15 @@ package hn.com.jf.models;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 import lombok.Data;
 
+@Component
 @Data
 public class Invoice {
+	@Autowired
 	private Client client;
 	private String description;
 	private List<Item> items;
