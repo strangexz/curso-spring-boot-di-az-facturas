0c991e6e53aa42e34d86ed52b60fa3561331ba1a
Strangexz <ja_figueroa@outlook.es>
2024-08-19 16:12:54 -0600

    config: configuraciones iniciales

---
+++ src/main/java/hn/com/jf/CursoSpringBootDIAzFacturasApplication.java
@@ -0,0 +1,13 @@
+package hn.com.jf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoSpringBootDIAzFacturasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoSpringBootDIAzFacturasApplication.class, args);
+	}
+
+}
